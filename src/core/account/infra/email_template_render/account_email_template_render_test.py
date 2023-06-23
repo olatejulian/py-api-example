c9043675720ad4e76a7b19bbc291d363b600950b
olatejulian <julian.lf.olate@gmail.com>
2023-06-23 00:15:24 -0300

    feat(account): :sparkles: add account email template renderer (adapter)

---
+++ src/core/account/infra/email_template_render/account_email_template_render_test.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+
+from src.core.account.domain import AccountEmailTemplateRender, Name, Url
+
+
+def test_account_template_render_render_email_verification_template(
+    account_email_template_render: AccountEmailTemplateRender,
+):
+    """
+    should be able to render a template with the provided URL.
+    """
+    render = account_email_template_render
+
+    email_verification_url = Url(
+        "https://a-wonderful-webapp-name.com/verifiy/123456789"
+    )
+
+    account_name = Name("John Doe)")
+
+    template = render.render_email_verification_code_template(
+        account_name=account_name,
+        email_verification_url=email_verification_url,
+    )
+
+    html = template.html_content.value
+
+    print(html)
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    account_name_in_template = soup.find("b", {"class": "account-name"})
+
+    assert account_name_in_template is not None
+
+    assert account_name_in_template.text == account_name
+
+    email_verification_url_in_template = soup.find_all(
+        "a", {"class": "email-verification-url"}
+    )
+
+    for tag in email_verification_url_in_template:
+        assert tag["href"] == email_verification_url
---
+++ src/core/account/infra/email_template_render/jinja2_account_email_template_render.py
@@ -0,0 +1,49 @@
+from jinja2 import Environment, FileSystemLoader
+
+from src.core.account.domain import (
+    AccountEmailTemplateRender,
+    EmailContent,
+    EmailContents,
+    Name,
+    Url,
+)
+from src.core.shared import EmailTemplateConfig
+
+
+class Jinja2AccountEmailTemplateRender(AccountEmailTemplateRender):
+    def __init__(self, config: EmailTemplateConfig):
+        self.config = config
+
+        self._env = Environment(loader=FileSystemLoader(self.config.template_dir))
+
+    def render_email_verification_code_template(
+        self, account_name: Name, email_verification_url: Url
+    ) -> EmailContents:
+        html_content, plaintext_content = [
+            self.__render_template(
+                account_name=account_name.value,
+                email_verification_url=email_verification_url.value,
+                template_name=template_name,
+            )
+            for template_name in [
+                self.config.email_verification_html_template,
+                self.config.email_verification_plaintext_template,
+            ]
+        ]
+
+        return EmailContents(
+            html_content=html_content,
+            plaintext_content=plaintext_content,
+        )
+
+    def __render_template(
+        self, account_name: str, email_verification_url: str, template_name: str
+    ) -> EmailContent:
+        template = self._env.get_template(template_name)
+
+        return EmailContent(
+            template.render(
+                account_name=account_name,
+                email_verification_url=email_verification_url,
+            )
+        )
