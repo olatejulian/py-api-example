e0f3522bd3ceddada709d7bb4cb41018e138fd95
olatejulian <julian.lf.olate@gmail.com>
2023-06-23 00:11:24 -0300

    feat(shared): :sparkles: add shared kernel entity mixin

---
+++ src/core/shared/entity_test.py
@@ -0,0 +1,25 @@
+from .entity import Entity
+from .interface import Event
+
+EVENT_NAME = "entity.created"
+
+
+class SimpleEvent(Event):
+    __event_name = EVENT_NAME
+
+    @classmethod
+    def get_event_name(cls) -> str:
+        return cls.__event_name
+
+
+class SimpleEntity(Entity):
+    def __init__(self):
+        self._events = []
+        self._add_event(SimpleEvent())
+
+
+def test_entity_collect_events():
+    entity = SimpleEntity()
+    events = list(entity.collect_events())
+    assert len(events) == 1
+    assert events[0].get_event_name() == EVENT_NAME
