dabb681b5b51fd5c8e2b84f3277ed2c9fe331b68
olatejulian <julian.lf.olate@gmail.com>
2023-06-28 00:03:18 -0300

    test(app): :white_check_mark: add verify email e2e test

--- src/app/_fastapi/bootstrap_e2e_test.py
+++ src/app/_fastapi/bootstrap_e2e_test.py
@@ -1,11 +1,33 @@
+import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
+from src.account import Account, AccountRepository
+from src.shared import DatabaseConfig
 
 from .bootstrap import fastapi_bootstrap
-from .dependency_injector import celery_event_bus_factory, fake_event_bus_factory
+from .dependency_injector import (
+    beanie_account_repository_factory,
+    celery_event_bus_factory,
+    database_config_factory,
+    fake_event_bus_factory,
+)
+
+
+def override_database_config_factory() -> DatabaseConfig:
+    config = DatabaseConfig()
+
+    config.name = "test-py-api-example"
+
+    return config
+
 
 app = fastapi_bootstrap()
 
+app.dependency_overrides[database_config_factory] = override_database_config_factory
+
 app.dependency_overrides[celery_event_bus_factory] = fake_event_bus_factory
+
 
 client = TestClient(app)
 
@@ -24,3 +46,40 @@ def test_signup():
     # then
     assert response.status_code == 200
     assert response.json() == {"success": True}
+
+
+@pytest.mark.asyncio
+async def test_verify_email(
+    random_account_fixture: Account, account_repository: AccountRepository
+):
+    """
+    should be able to verify an email using query params (email to be verified and it token)
+    """
+    # given
+    entity = random_account_fixture
+
+    repository = account_repository
+
+    def override_account_repository_factory() -> AccountRepository:
+        return repository
+
+    app.dependency_overrides[
+        beanie_account_repository_factory
+    ] = override_account_repository_factory
+
+    verification_code = entity.generate_verification_code()
+
+    query_params = {
+        "email": entity.email.address.value,
+        "token": verification_code.value,
+    }
+
+    # when
+    async with AsyncClient(app=app, base_url="http://localhost:3000") as async_client:
+        await repository.save(entity)
+
+        response = await async_client.post("/verify", params=query_params)
+
+        # then
+        assert response.status_code == 200
+        assert response.json() == {"success": True}
