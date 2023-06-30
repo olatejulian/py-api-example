89d4a6bf35279490f50889afcff7a139f3adc448
olatejulian <julian.lf.olate@gmail.com>
2023-06-30 03:22:06 -0300

    feat(app): :sparkles: add resend verification email sub path

--- src/app/router/verify_email_router.py
+++ src/app/router/verify_email_router.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, Depends
 
-from src.account import EmailAddress, VerificationCode, VerifyAccountEmail
+from src.account import (
+    EmailAddress,
+    ResendVerificationEmail,
+    VerificationCode,
+    VerifyAccountEmail,
+)
 from src.shared import CommandBus
 
 from ..dependency_injector import command_bus_factory
@@ -18,7 +23,7 @@ class VerifyEmailResponse(HTTPResponseModel[dict]):
         )
 
 
-verifyEmailRouter = APIRouter(tags=["public"])
+verifyEmailRouter = APIRouter(tags=["public", "verify email"])
 
 
 @verifyEmailRouter.post("/verify", response_model=VerifyEmailResponse)
@@ -30,6 +35,33 @@ async def verify_email(
     await command_bus.dispatch(
         VerifyAccountEmail(email=EmailAddress(email), token=VerificationCode(token))
     )
+
+    return VerifyEmailResponse(
+        status_code=200,
+        data={},
+        message=verify_email_response_message(),
+    )
+
+
+def resend_verification_email_response_message() -> str:
+    return "Verification email resent successfully."
+
+
+class ResendVerificationEmailResponse(HTTPResponseModel[dict]):
+    class Config:
+        schema_extra = SchemaExtraConfig.override_schema_extra_example(
+            data={}, message=resend_verification_email_response_message()
+        )
+
+
+@verifyEmailRouter.post("verify/resend/{email}")
+async def resend_verification_email(
+    email: str,
+    command_bus: CommandBus[ResendVerificationEmail, None] = Depends(
+        command_bus_factory
+    ),
+):
+    await command_bus.dispatch(ResendVerificationEmail(email=EmailAddress(email)))
 
     return VerifyEmailResponse(
         status_code=200,
